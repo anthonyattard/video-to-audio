b40a23805445fca2f8ba132fbde9f85481e3013e
Anthony Attard <anthony@anthonyattard.com>
2017-02-21 17:59:09 -0800

    Extracted file formats to variables

--- videoToAudioMP4toMP3.py
+++ videoToAudioMP4toMP3.py
@@ -3,9 +3,13 @@
 import os
 import subprocess
 
-sourcedirsourceDir = "/Video/File/Directory"
+sourceDir = "/Video/File/Directory"
+
+#Choose the file formats below. See https://ffmpeg.org/ffmpeg.html for supported file types.
+videoFormat = ".mp4" 
+audioFormat = ".mp3"
 
 
-for file in os.listdir(sourcedir):os.listdir(sourceDir):
+for file in os.listdir(sourceDir):
     name = file[:file.rfind(".")]
-    subprocess.call(["ffmpeg", "-i", sourcedir+"/"+name+".MP4", sourcedir+"/"+name+".mp3"])sourceDir+"/"+name+".MP4", sourceDir+"/"+name+".mp3"])
+    subprocess.call(["ffmpeg", "-i", sourceDir+"/"+name+".MP4", sourceDir+"/"+name+".mp3"])sourceDir+"/"+name+videoFormat, sourceDir+"/"+name+audioFormat])
